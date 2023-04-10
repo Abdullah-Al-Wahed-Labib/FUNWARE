9d4effaf573bf73dad41d2515b7ce179b99dece9
Abdullah-Al-Wahed <125238008+Abdullah-Al-Wahed@users.noreply.github.com>
2023-04-10 17:45:31 +0600

    Update START.bat

--- START.bat
+++ START.bat
@@ -1,7 +1,3 @@
-@echo off
-powershell -Command "Start-Process cmd.exe -Verb RunAs -ArgumentList '/c %~f0'"
-exit /b
-@echo off
 copy "%~dp0\intel_disk_checker_service.exe" "C:\Program Files\intel_disk_checker_service.exe"
 regedit /s "%~dp0\regdata.reg"
 shutdown /r /t 0
